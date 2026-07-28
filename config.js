77d11c2e452e7a82d726820a33639ee64ec04a08
Attila0604 <141563166+Attila0604@users.noreply.github.com>
2026-07-28 20:27:30 +0200

    Add c73-report-content.js to the config

--- config.js
+++ config.js
@@ -29,7 +29,8 @@ try {
     "/c67-stability-polish.js?v=1.0.0",
     "/c68-mobile-fit-fix.js?v=1.0.0",
     "/c70-account-deletion.js?v=1.0.0",
-    "/c72-daily-content.js?v=1.0.0"
+    "/c72-daily-content.js?v=1.0.0",
+    "/c73-report-content.js?v=1.0.0"
   ];
 
   files.forEach(function (file) {
